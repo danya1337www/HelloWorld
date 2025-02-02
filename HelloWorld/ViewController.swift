62b0ec1374dd48649dfb0b3f319ac50652277a7e
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-02-02 10:50:50 +0300

    First Project

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Danil Chekantsev on 02.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
